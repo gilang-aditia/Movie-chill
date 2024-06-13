3e1668ec80b470270fd8d585a1db827b5f8603df
Gilang <gaditia744@gmail.com>
2024-06-14 00:01:42 +0700

    kelima

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Profile from "../assets/img/Profile.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="absolute w-full p-4 flex justify-between items-center z-50 flex-wrap">
       <div className="flex items-center mb-2 md:mb-0">
-        {" "}
-        {/* Bagian kiri */}
         <Link to="/" className="mr-2 md:mr-8">
-          {/* masukan gambar */}
           <img
             src="https://i.ibb.co.com/x2PHnJM/Logo-chill.png"
             alt="logo"
@@ -16,8 +19,6 @@ const Navbar = () => {
           />
         </Link>
         <div className="flex flex-wrap">
-          {" "}
-          {/* Menu navigasi */}
           <Link to="/series" className="mr-2 md:mr-4 mb-2 md:mb-0">
             <button className="capitalize text-sm md:text-base px-2 md:px-4 py-1 md:py-2">
               Series
@@ -36,23 +37,37 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex items-center">
-        {" "}
-        {/* Bagian kanan */}
-        <div className="flex">
-          {" "}
-          {/* Tombol Login dan Sign Up */}
-          <Link to="/login">
-            <button className="capitalize text-sm md:text-base pr-2 md:pr-4">
-              Login
+      <div className="relative">
+        <div className="flex items-center">
+          <div className="flex">
+            <button
+              className="relative flex items-center justify-center w-10 h-10 bg-gray-300 rounded-full overflow-hidden focus:outline-none"
+              onClick={toggleDropdown}>
+              <img
+                src={Profile}
+                alt="Profile"
+                className="w-full h-full object-cover"
+              />
             </button>
-          </Link>
-          <Link to="/signup">
-            <button className="capitalize text-sm md:text-base bg-red-600 px-3 md:px-6 py-1 md:py-2 rounded cursor-pointer">
-              Sign up
-            </button>
-          </Link>
+          </div>
         </div>
+
+        {isOpen && (
+          <div className="absolute right-0 mt-2 w-48 bg-gray-900 rounded-lg shadow-lg overflow-hidden z-10">
+            <div className="py-1">
+              <Link to="/login">
+                <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-700 focus:outline-none">
+                  Login
+                </button>
+              </Link>
+              <Link to="/signup">
+                <button className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-700 focus:outline-none">
+                  Sign up
+                </button>
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
